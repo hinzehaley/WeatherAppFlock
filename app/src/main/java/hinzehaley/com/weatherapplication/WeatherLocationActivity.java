eb5713f83728753adad8a2bf84afecf563f8666b
hinzehaley <hinzehaley@gmail.com>
2017-05-03 13:13:07 -0600

    initial commit

---
+++ app/src/main/java/hinzehaley/com/weatherapplication/WeatherLocationActivity.java
@@ -0,0 +1,13 @@
+package hinzehaley.com.weatherapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_location);
+    }
+}
